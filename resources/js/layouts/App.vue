e1d32bbab95e3a8ab01ee0c058803021894c6f6e
FernandoStefanutto <fernandostefanutto@MacBook-Air-de-Fernando.local>
2023-04-27 08:47:56 -0300

    Init

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="text-center">Hello App!</h1>
+        <p class="text-center">
+            <router-link to="/" class="mx-1">Go to Home</router-link>
+            <router-link to="/about" class="mx-1">Go to About</router-link>
+        </p>
+
+        <div class="text-center">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/store/teste.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+// Create a new store instance.
+const store = createStore({
+    state: {
+        teste: 'Store de teste',
+    },
+    getters: {},
+    actions: {
+    },
+    mutations: {
+    },
+    modules: {},
+})
+
+export default store
